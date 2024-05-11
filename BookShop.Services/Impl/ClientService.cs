c1ae3529d9fc18c5fbdb3fd315080cdee3548b78
Aram Baghdyan <aram.baghdyan@gmail.com>
2024-05-11 22:48:44 +0400

    Add simple hashing for password

--- BookShop.Services/Impl/ClientService.cs
+++ BookShop.Services/Impl/ClientService.cs
@@ -99,18 +99,9 @@ internal class ClientService : IClientService
 
     private string HashPassword(string password)
     {
-        using (SHA256 sha256 = SHA256.Create())
-        {
-            byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-            StringBuilder builder = new StringBuilder();
-
-            for (int i = 0; i < hashedBytes.Length; i++)
-            {
-                builder.Append(hashedBytes[i].ToString("x2"));
-            }
-
-            return builder.ToString();
-        }
+        var passwordBytes = Encoding.UTF8.GetBytes(password);
+        var passwordHash = SHA256.HashData(passwordBytes);
+        return Convert.ToHexString(passwordHash);
     }
 
     public async Task<ClientGetVm> GetByIdAsync(long clientId)
