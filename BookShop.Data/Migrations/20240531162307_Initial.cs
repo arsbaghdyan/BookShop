ee2783d971239629cd864aaf8be438f5938c1d09
Aram Baghdyan <aram.baghdyan@gmail.com>
2024-05-31 20:23:21 +0400

    Add migration

---
+++ BookShop.Data/Migrations/20240531162307_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookShop.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InvoiceStatus",
+                table: "Invoices",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvoiceStatus",
+                table: "Invoices");
+        }
+    }
+}
